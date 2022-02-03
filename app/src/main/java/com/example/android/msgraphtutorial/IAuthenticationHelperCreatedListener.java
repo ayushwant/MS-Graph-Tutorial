5e637ad50b7544773ed7aad3cad7d882aff5f87d
ayushwant <ayushwant912@gmail.com>
2022-02-03 20:20:19 +0530

    first commit till sign in

---
+++ app/src/main/java/com/example/android/msgraphtutorial/IAuthenticationHelperCreatedListener.java
@@ -0,0 +1,8 @@
+package com.example.android.msgraphtutorial;
+
+import com.microsoft.identity.client.exception.MsalException;
+
+public interface IAuthenticationHelperCreatedListener {
+    void onCreated(final AuthenticationHelper authHelper);
+    void onError(final MsalException exception);
+}
